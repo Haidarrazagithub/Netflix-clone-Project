c9883439e67477200d6140956329f7a148c91b85
Haidarraza Meghreji <haidarraza.meghreji@bksd.com>
2022-08-23 14:51:22 +0530

    First

---
+++ django_netflix_clone-main/core/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+# from .models import CustomUser,Profile,Movie,Video
+
+# Register your models here.
+from django.apps import apps
+from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
+
+app_models = apps.get_app_config('core').get_models()
+
+for model in app_models:
+    try:
+        class ModelAdmins(admin.ModelAdmin):
+            all_fields = [i.name for i in model._meta.fields]
+            list_display = [item for item in all_fields ]
+            search_fields = [i.name for i in model._meta.fields if i.get_internal_type() not in ['OneToOneField', 'ForeignKey']]
+            list_per_page = 50
+            list_max_show_all = 500
+            parent_fields = model.get_deferred_fields(model)
+
+
+        admin.site.register(model, ModelAdmins)
+    except AlreadyRegistered:
+        pass
